7a883e19bd62c8aa21c4dc3b70a3d7753d0b9abb
Glecios <glleciosyuri@gmail.com>
2025-08-10 00:08:19 -0300

    criação do component tasks

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CategoryComponent } from '../../feature/catefories/view/category/category.component';
+import {MatDividerModule} from '@angular/material/divider';
 
 const COMPONENTS = [CategoryComponent];
+const MODULES = [MatDividerModule];
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [...COMPONENTS],
+  imports: [...COMPONENTS,...MODULES],
   templateUrl: './main.component.html',
   styles: ``
 })
