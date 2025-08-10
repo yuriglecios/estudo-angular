5f5f829cff570daec4a404a81d3375081419acb1
Glecios <glleciosyuri@gmail.com>
2025-08-09 23:27:44 -0300

    criando o component main e ajustando o app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ThemeToggleComponent } from './shared/components/theme-toggle/theme-toggle.component';
 import { ThemeService } from './shared/services/theme.service';
+import { MainComponent } from './layout/main/main.component';
+
+const COMPONENTS = [
+  ThemeToggleComponent,
+  MainComponent
+];
+
+const MODULES = [];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ThemeToggleComponent],
+  imports: [...COMPONENTS],
   template: `
     <div class="relative min-h-screen w-full">
       <app-theme-toggle />
+
+      <app-main/>
     </div>
   `,
   styles: ``,
