b2dacbe39c4760512cd3076f441b23bbfb5e71a5
Glecios <glleciosyuri@gmail.com>
2025-08-10 22:04:46 -0300

    criando os componentes que usaremos para a área de categorias

---
+++ src/app/feature/catefories/component/categories-colors/categories-colors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-colors',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories-colors.component.html',
+  styles: ``
+})
+export class CategoriesColorsComponent {
+
+}
